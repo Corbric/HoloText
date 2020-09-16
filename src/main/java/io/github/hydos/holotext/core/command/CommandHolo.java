4d858a8b43b200b3e9b5b1ddd3f63a1cb57a2162
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-16 18:50:14 +0530

    Removal
     Changes to be committed:
    	modified:   src/main/java/io/github/hydos/holotext/core/command/CommandHolo.java

--- src/main/java/io/github/hydos/holotext/core/command/CommandHolo.java
+++ src/main/java/io/github/hydos/holotext/core/command/CommandHolo.java
@@ -1,5 +1,7 @@
 package io.github.hydos.holotext.core.command;
 
+import java.util.Objects;
+
 import io.github.hydos.holotext.HoloText;
 import io.github.hydos.holotext.core.EntityHoloText;
 import com.google.common.collect.Lists;
@@ -7,8 +9,10 @@ import com.google.common.collect.Lists;
 import net.minecraft.command.AbstractCommand;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.CommandSource;
+import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.LiteralText;
+import net.minecraft.util.math.BlockPos;
 
 public class CommandHolo extends AbstractCommand {
     @Override
@@ -43,6 +47,21 @@ public class CommandHolo extends AbstractCommand {
                             sourceIn.sendMessage(new LiteralText("Successfully added HoloText at position " + entity.getPos().toString()));
                             HoloText.CONFIG.getEntryMap().put(name, entity.getEntry());
                             HoloText.serialize();
+                        }
+                    } else if (theArg.equals("remove")) {
+                        String name = argsIn[1];
+                        if (name == null) {
+                            throw new CommandException("command.holo.error");
+                        } else {
+                            if (!HoloText.CONFIG.getEntryMap().containsKey(name)) {
+                                throw new CommandException("command.holo.error");
+                            } else {
+                                if (sourceIn.getWorld().isLoaded(new BlockPos(Objects.requireNonNull(HoloText.CONFIG.getEntryMap().get(name)).getPosVec()), true)) {
+                                    sourceIn.getWorld().entities.removeIf((entity) -> entity instanceof ArmorStandEntity && entity.getPos().equals(Objects.requireNonNull(HoloText.CONFIG.getEntryMap().get(name)).getPosVec()));
+                                }
+                                HoloText.CONFIG.getEntryMap().remove(name);
+                                HoloText.serialize();
+                            }
                         }
                     }
                 }
